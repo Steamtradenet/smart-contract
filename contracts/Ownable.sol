80628fb3c6449d87cfc71fd09c1adb40e22bebad
Alexander Kravin <krsasha2004@gmail.com>
2017-06-15 15:47:42 +0300

    Improve check for zero address

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -13,8 +13,8 @@ contract Ownable {
     }
 
     function transferOwnership(address newOwner) onlyOwner {
-        if (newOwner == address(0)) throw;
-
-        owner = newOwner;
+        if (newOwner != address(0)) {
+            owner = newOwner;
+        }
     }
 }
